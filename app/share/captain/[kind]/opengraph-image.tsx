588fc44589379b975645fe99ac73f1daca875d80
Nathan Meinert <nmeinert91@gmail.com>
2026-09-14 21:39:23 -0500

    Keep share preview lint clean

--- app/share/captain/[kind]/opengraph-image.tsx
+++ app/share/captain/[kind]/opengraph-image.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 import { ImageResponse } from 'next/og'
 import { getCaptainShareConfig, isCaptainShareKind, type CaptainShareKind } from '@/lib/captain-share-preview'
 
